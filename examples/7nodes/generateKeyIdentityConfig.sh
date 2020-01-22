588bb09881d1329895a0c791f50960769a6b1c5c
nicolae <nicolae.leonte.go@gmail.com>
2020-01-22 15:34:59 +0000

    fix keyIdentitySignature for mac (base64 not having the -w option)

--- examples/7nodes/generateKeyIdentityConfig.sh
+++ examples/7nodes/generateKeyIdentityConfig.sh
@@ -6,7 +6,7 @@ identityKeyName=$3
 identityPublicKey=$(cat ${DDIR}/$publicKeyName.pub)
 identityKey=$(cat ${DDIR}/$identityKeyName.sl.pub)
 
-timestamp=$(date +%s%3N)
+timestamp=$(date +%s000)
 echo timestamp: $timestamp
 timestampHex=$(printf "%016x" $timestamp)
 
@@ -15,7 +15,7 @@ echo $timestampHex | xxd -r -p >> $DDIR/dataToSign.bin
 
 openssl dgst -sha256 -sign $DDIR/$identityKeyName.pem -out $DDIR/signedData.bin $DDIR/dataToSign.bin
 
-base64Signature=$(base64 -w 0 $DDIR/signedData.bin)
+base64Signature=$(base64 $DDIR/signedData.bin | awk 'NF {sub(/\r/, ""); printf "%s",$0;}')
 
 cat <<EOF > ${DDIR}/keyIdentities.json
 [
