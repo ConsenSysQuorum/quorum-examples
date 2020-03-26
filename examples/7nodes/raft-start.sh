96c9805123cf1a3d13c36f66de5448ac36a41d95
vsmk98 <vsaimk@gmail.com>
2020-03-26 17:43:54 +0800

    corrected the scripts for state extension

--- examples/7nodes/raft-start.sh
+++ examples/7nodes/raft-start.sh
@@ -121,7 +121,7 @@ set -v
 ARGS="--nodiscover --allow-insecure-unlock --verbosity ${verbosity} --networkid $NETWORK_ID --raft --raftblocktime ${blockTime} --rpc --rpccorsdomain=* --rpcvhosts=* --rpcaddr 0.0.0.0 --rpcapi admin,eth,debug,miner,net,shh,txpool,personal,web3,quorum,raft,quorumPermission,quorumExtension --emitcheckpoints --unlock 0 --password passwords.txt $QUORUM_GETH_ARGS"
 
 basePort=21000
-baseRpcPort=32000
+baseRpcPort=22000
 baseRaftPort=50401
 for i in `seq 1 ${numNodes}`
 do
